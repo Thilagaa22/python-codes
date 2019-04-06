959b56415a31ce696265409aa0d5a7d45f7cad91
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-06 11:13:27 +0530

    numcheck.py

--- numcheck.py
+++ numcheck.py
@@ -1,7 +1,7 @@
 number = int(input("Enter the number"))
-if num % 2 == 0 :
+if (num % 2 == 0) :
   print("Number is Even")
-elif num % 2 != 0:
+elif (num % 2 != 0):
   print("Numbre is odd")
 else:
   print("Number is invaild")
