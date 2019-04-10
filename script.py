b337e862ef1e6757a7997b2fc318dcee7df84e1a
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-10 12:06:53 +0530

    script.py

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 lower = int(input("Enter a lower limit:"))
 upper = int(input("Enter a upper limit:"))
 print("even numbers between",lower ,"and", upper ,"are:")
-for num in range(lower,upper +1):
+for num in range(lower,upper):
   if num >1:
     for i in range(2,num):
-      if(num % i) == 0:
+      if(num % 2) != 0:
         break
     else:
       print(num)
