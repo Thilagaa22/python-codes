b15b34a05837d031f10d86b7459a2b8241920f0f
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-11 12:20:59 +0530

    min.py

--- min.py
+++ min.py
@@ -3,4 +3,4 @@ n = int(input("how many numbers:"))
 for x in range(n):
   num = int(input('Enter number:'))
   a.append(num)
-print("minmum number in the list:",min(lst))
+print("minmum number in the list:",min(a))
