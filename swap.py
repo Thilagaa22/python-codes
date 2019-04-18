558dcb07429660206edef09dbfc37d7373a1f186
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-18 16:24:50 +0700

    swap.py

---
+++ swap.py
@@ -0,0 +1,7 @@
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+temp = x
+x = y
+y = temp
+print('The value of x after swap:{}',format(x))
+print('The value of y after swap:{}',format(y))
