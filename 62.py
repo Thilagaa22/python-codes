c5823cc2594c4573a66968ea12fc98a92f71c04e
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-20 22:03:34 +0700

    62.py

--- 62.py
+++ 62.py
@@ -1,10 +1,6 @@
-def check(str):
-  p = set(str)
-  s={'0','1'}
-  if s == p or p == {'0'} or p =={'1'}:
-    print("yes")
-  else:
-    print("No")
-if __name__ == "__main__":
-  str = int(input())
-  check(string)
+import re
+b = input()
+if re.findall("[a-zA-Z2-9]",b):
+  print("No")
+else:
+  print("Yes")
