73877c8abacd55775448e422f7538fc565034541
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-21 12:02:29 +0700

    72.py

--- 72.py
+++ 72.py
@@ -1,5 +1,5 @@
 ch = input()
 if (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch =='u' or ch == 'A' or ch == 'E' or ch == 'I' ch =='O' or ch=='U'):
-  print("vowels")
+  print("Yes")
 else:
   print("No")
