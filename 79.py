0faa09abff63afdc9e159f2397233f7a93176dc3
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-21 16:25:53 +0700

    79.py

--- 79.py
+++ 79.py
@@ -1,9 +1,9 @@
 import math
-def isPerfectSquare(x):
+def square(x):
   sr = math.sqrt(x)
   return((sr - math.floor(sr)) == 0 )
 x = int()
-if (isPerferctSquare(x)):
+if (square(x)):
   print("Yes")
 else:
   print("No")
