91b807e3094d49ce6b3906c79b1ae995d1e8f563
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-21 23:19:40 +0700

    24.py

---
+++ 24.py
@@ -0,0 +1,8 @@
+def sort(arr,n):
+  for i in range(n):
+    if __name__=='__main__':
+      arr = int(input())
+      n = len(arr)
+      sort(arr,n)
+      for i in range(n):
+        print(arr[i])
