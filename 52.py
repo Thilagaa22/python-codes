e67a7b75ae5f2a2ba02fd69a4a560f2fc4b97bca
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-22 15:59:54 +0530

    52.py

---
+++ 52.py
@@ -0,0 +1,23 @@
+n = int(inpit())
+if n == 1:
+  print("one")
+elif n == 2:
+  print("two")
+elif n == 3:
+  print("Three")
+elif n == 4:
+  print("Four")
+elif n == 5:
+  print("five")
+elif n==6:
+  print("six")
+elif n==7:
+  print("seven")
+elif n==8:
+  print("Eight")
+elif n==9:
+  print("Nine")
+elif n == 10:
+  print("ten")
+else:
+print("Invaild")
