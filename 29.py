a267c4937c30b15bdeacfbb32e79aa7d9c90c05e
Thilagaa22 <49054409+Thilagaa22@users.noreply.github.com>
2019-04-23 14:04:48 +0530

    29.y

--- 29.py
+++ 29.py
@@ -1,4 +1,4 @@
-time = float(input("Time in seconds:"))
+time = int(input("Time in seconds:"))
 print("")
 day = time // (24 * 3600)
 hour = time % (24 * 3600)
@@ -6,4 +6,4 @@ hour = time // 3600
 minutes = time // 60
 time % = 60
 seconds = time
-print(hour,seconds))
+print(minutes,seconds))
